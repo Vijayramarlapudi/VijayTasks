3cb73df660240010c533c1e09f195581cebb7f0d
Vijayramarlapudi <116052001+Vijayramarlapudi@users.noreply.github.com>
2022-11-10 18:47:47 +0530

    Commit

---
+++ Classes/18-09Nov2022/18-09Nov2022/create_Object.js
@@ -0,0 +1,32 @@
+// var bank= {
+//     name: "icici",
+//     ifscode: 'icici087677',
+//     location: "madhapur"
+// }
+
+//  var customer=Object.create(bank);
+
+//  console.log(customer);
+
+//  customer.username="ravikumar";
+//  customer.accno="1234455";
+//  customer.balance=9000;
+
+//  console.log(customer);
+//  console.log(customer.name);
+//  console.log(customer.ifscode);
+
+
+// var config=Object.freeze( {
+//     port:9011,
+//     url:"http://www.facebook.com",
+//     id:1244
+// })
+
+// console.log('config', config);
+
+// config.port=9799;
+
+// console.log('config', config)
+
+// console.log(Object.isFrozen(config));
---
+++ Classes/19-10Nov2022/19-10Nov2022/scopes.js
@@ -0,0 +1,7 @@
+var global = 'this is global variable';
+
+function f1(){
+
+    local = 'this is local variable'
+
+}
